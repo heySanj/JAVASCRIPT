72afe0747cc8b0e94059a74f60faf0072fb7e3f7
heySanj <saprasad92@gmail.com>
2022-11-21 15:24:20 +1000

    Express Sessions and Flash

---
+++ express-session/SessionDemo/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+
+// Set session options based on what store method is being used (ie: Redis)
+// Make sure to change the memory store setting when deploying the app! Default memory store is just for production/testing
+const sessionOptions = { secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false }
+app.use(session(sessionOptions));
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You have viewed this page ${req.session.count} times`)
+})
+
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`)
+})
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
